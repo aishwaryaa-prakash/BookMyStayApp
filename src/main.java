fcd3a1939ecfdb6f55edbf234b4e146b4806f63e
Aishwarya Prakash <aishwaryaaprakash88@gmail.com>
2026-03-18 10:36:01 +0530

    Initialize BookMyStayApp with main method

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
-/**
- * UseCase1HotelBookingApp
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        String appName = "BookMyStay - Hotel Booking System";
+        String version = "v1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println("Application Version: " + version);
+    }
+}
